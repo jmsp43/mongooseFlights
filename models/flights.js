75c150e2f474d3e6f2bb6e948ed67ab23048eddc
Jenna Perez <jmsp43@gmail.com>
2023-05-15 19:48:36 -0400

    added dependencies and flight model

---
+++ models/flights.js
@@ -0,0 +1,22 @@
+const flights = [
+     {
+          flightNum: 123456,
+          pilot: 'Julia Fernandez',
+          model: 'Boeing 747',
+          inAir: true
+     },
+     {
+          flightNum: 78901,
+          pilot: 'Quasia Smith',
+          model: 'P&W 1000',
+          inAir: true
+     },
+     {
+          flightNum: 24680,
+          pilot: 'Andy Kim',
+          model: 'Boeing 747',
+          inAir: false
+     }
+]
+
+module.exports = flights
--- server.js
+++ server.js
@@ -1,20 +1,31 @@
-const express = require('express')
+const express = require("express");
+const mongoose = require("mongoose");
+const flights = require("./models/flights");
+require("dotenv").config();
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+
+const mongoURI = process.env.MONGO_URI;
+const db = mongoose.connection;
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+db.on("error", (err) => console.log(err.message + " is mongod not running?"));
+db.on("open", () => console.log("mongo connected: ", mongoURI));
+db.on("close", () => console.log("mongo disconnected"));
 
 app.use((req, res, next) => {
-     console.log('I run for all routes');
-     next();
- });
- app.use(express.urlencoded({extended:false}));
- app.set('view engine', 'jsx');
- app.engine('jsx', require('jsx-view-engine').createEngine());
+  console.log("I run for all routes");
+  next();
+});
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "jsx");
+app.engine("jsx", require("jsx-view-engine").createEngine());
 
-app.get('/', (req, res) => {
-     res.send('Every little thing is gonna be alright')
-})
+app.get("/", (req, res) => {
+  res.send("Every little thing is gonna be alright");
+});
 
 app.listen(port, () => {
-     console.log(`Don't worry, be happy`)
-})
+  console.log(`Don't worry, be happy`);
+});
