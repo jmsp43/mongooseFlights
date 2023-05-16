f183a38a4a683b035478145cce688c6fe302c933
Jenna Perez <jmsp43@gmail.com>
2023-05-15 20:59:24 -0400

    deleted extraneous code

--- schemas/flight.js
+++ schemas/flight.js
@@ -1,10 +1,9 @@
-const mongoose = require("mongoose"); // require mongoose
-const Schema = mongoose.Schema; // create a shorthand for the mongoose Schema constructor
-const model = mongoose.model // shorthand for model function
+const mongoose = require("mongoose"); 
 
-// create a new Schema
-// This will define the shape of the documents in the collection
-// https://mongoosejs.com/docs/guide.html
+const Schema = mongoose.Schema; 
+
+const model = mongoose.model 
+
 const flightSchema = new Schema(
   {
           num: {
@@ -26,12 +25,7 @@ const flightSchema = new Schema(
   }
 );
 
-// Creating Tweet model : We need to convert our schema into a model-- will be stored in 'tweets' collection.  Mongo does this for you automatically
-// Model's are fancy constructors compiled from Schema definitions
-// An instance of a model is called a document.
-// Models are responsible for creating and reading documents from the underlying MongoDB Database
-// from here: https://mongoosejs.com/docs/models.html
+
 const Flight = model("Flight", flightSchema);
 
-//make this exportable to be accessed in `app.js`
 module.exports = Flight;
